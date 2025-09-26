f11612204b5201cc0e1d225d1853d71340753b94
Diane-4P <f39xy05z@students.codeinstitute.net>
2025-09-26 17:52:06 +0100

    Add function for add_book_a_table

--- book_a_table/views.py
+++ book_a_table/views.py
@@ -1,7 +1,44 @@
+""" Imports from django, book_a_table models and forms """
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from .models import BookATable, BookingDetails
+from .forms import BookATableForm, BookingDetailsForm
 
 
 # Create your views here.
-def book_a_table(request):
-    return HttpResponse('Book a table page coming soon!')
+def add_book_a_table(request):
+    """ View to handle table booking form """
+    
+    if request.method == 'POST':
+        if request.user.is_superuser or not request.user.is_authenticated:
+            messages.error(request, 'You must be logged in to book a table.')
+            return render(request, 'book_a_table/book_a_table.html', {'form': BookATableForm()})
+        form = BookATableFormAdmin(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            try:
+                booking.validate_date()
+                booking.save()
+                messages.success(request, 'Table booked successfully!')
+            except ValidationError as e:
+                form.add_error('booking_date', e.message)
+        else:        
+            form = BookATableForm(request.POST)
+            if form.is_valid():
+                booking = form.save(commit=False)
+                booking.user = request.user
+                try:
+                    booking.validate_date()
+                    booking.save()
+                    messages.success(request, 'Table booked successfully!')
+                except ValidationError as e:
+                    form.add_error('booking_date', e.message)
+    else:
+        if request.user.is_superuser:
+            form = BookATableFormAdmin()
+        else:
+            form = BookATableForm()
+    
+    return render(request, 'book_a_table/book_a_table.html', {'form': form})
+
