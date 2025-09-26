99a0d2be0ee68deed0caa8d045075ada027c2dc1
Diane-4P <f39xy05z@students.codeinstitute.net>
2025-09-26 13:16:29 +0100

    Add model book_a_table to admin.py to register

--- book_a_table/admin.py
+++ book_a_table/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BookATable, BookingDetails
 
 # Register your models here.
+admin.site.register(BookATable)
+admin.site.register(BookingDetails)
