38db10a8c1b6139c2b08b83407824f2fd022f659
Diane-4P <f39xy05z@students.codeinstitute.net>
2025-09-29 23:12:12 +0100

    Add created_on and approved to list display in admin.py

--- book_a_table/admin.py
+++ book_a_table/admin.py
@@ -1,18 +1,15 @@
-"""Imports from django and book_a_table models"""
+"""Imports from django and BookATable models"""
 from django.contrib.admin import ModelAdmin
 from django.contrib import admin
 from .models import BookATable
 
 
 # Register your models here.
-admin.site.register(BookATable)
-
-
 @admin.register(BookATable)
 class BookATableAdmin(ModelAdmin):
     """ Admin view for BookATable model """
     
-    list_display = ('book_table_id', 'user', 'first_name', 'last_name', 'email', 'phone_number', 'booking_date', 'booking_time', 'number_of_guests' 'special_requests', 'occasion')
+    list_display = ('book_table_id', 'user', 'first_name', 'last_name', 'email', 'phonenumber', 'booking_date', 'booking_time', 'number_of_guests', 'special_requests', 'occasion', 'created_on', 'approved')
     ordering = ('-booking_date', '-booking_time')
     search_fields = ('user__username', 'booking_date')
     list_filter = ('booking_date', 'booking_time')
