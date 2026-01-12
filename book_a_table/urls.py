84a51a7476780882e870a09bfdd6589686704177
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-01-12 23:30:01 +0000

    Update urlpatterns for booking

--- book_a_table/urls.py
+++ book_a_table/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 # URL patterns for book_a_table app
 urlpatterns = [
-    path('bookings', views.bookings, name='bookings'),
-    path('book_a_table/', views.book_a_table, name='book_a_table'),
-    path('edit/<int:book_table_id>/', views.edit_booking, name='edit_booking'),
-    path('delete/<int:book_table_id>/', views.delete_booking, name='delete_booking'),
+    path('', views.book_a_table, name='book_a_table'),
+    path('bookings/', views.bookings, name='bookings'),
+    path('edit_bookings/<int:booking_id>/', views.edit_bookings, name='edit_bookings'),
+    path('delete_bookings/<int:booking_id>/', views.delete_bookings, name='delete_bookings'),
 ]
