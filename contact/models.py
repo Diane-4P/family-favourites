101c3a23e4eda5a324b89ef16278f8bc570d6748
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-01-23 16:41:26 +0000

    Make all fields required to be filled in

--- contact/models.py
+++ contact/models.py
@@ -5,11 +5,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class Contact(models.Model):
     contact_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False, unique=True)
     phonenumber = PhoneNumberField(unique=True, null=False, blank=False, max_length=13, default='+441234567890')    
-    message = models.TextField(blank=True, null=True)
+    message = models.TextField(blank=False, null=False)
 
     def __str__(self):
         return f'You will be contacted via {self.email} or {self.phonenumber} relating to your query as soon as possible.'
