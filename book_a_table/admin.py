9e1fa1a1375bd7b91c756478d70f83840f976f53
Diane-4P <f39xy05z@students.codeinstitute.net>
2026-01-30 14:27:14 +0000

    Add book table id to ordering for admin

--- book_a_table/admin.py
+++ book_a_table/admin.py
@@ -10,7 +10,7 @@ class BookATableAdmin(ModelAdmin):
     """ Admin view for BookATable model """
     
     list_display = ('book_table_id', 'user', 'date', 'time', 'number_of_guests', 'special_requests', 'occasion', 'approved')
-    ordering = ('-date', '-time')
+    ordering = ('-book_table_id', '-date', '-time')
     search_fields = ('user__username', 'date')
     list_filter = ('date', 'approved')
     actions = ['booking_confirmed']
